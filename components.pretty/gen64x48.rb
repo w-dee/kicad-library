e32707e9d886cd7fc88fb1379f9558565fb1364a
W.Dee <dee@kikyou.info>
2016-11-10 10:11:18 +0900

    Updated library

--- components.pretty/gen64x48.rb
+++ components.pretty/gen64x48.rb
@@ -11,18 +11,26 @@ puts("
  ")
 for y in (0..47) do
 	for x in (0..63) do
-		xx = x*4
-		yy = y*4
+		xx = x*3.85
+		yy = y*3.85
 		pk = x + ((y&1)!=0? 64:0) + 1 
 		pa = (y >> 1) + 200 + 1
 		n = "
+  (fp_line (start -1.493898 0.129696) (end -1.493898 -0.170304) (layer F.SilkS) (width 0.15))
+  (fp_line (start -1.593898 0.129696) (end -1.593898 -0.170304) (layer F.SilkS) (width 0.15))
+  (fp_text user #{x},#{y} (at -0.3 -1.3) (layer F.SilkS)
+    (effects (font (size 0.6 0.6) (thickness 0.15)))
+  )
 
-  (fp_line (start -1.093898 0.129696) (end -1.093898 -0.170304) (layer F.SilkS) (width 0.15))
-  (fp_line (start -1.193898 0.129696) (end -1.193898 -0.170304) (layer F.SilkS) (width 0.15))
-  (pad #{pa} smd rect (at -0.293898 -0.020304 90) (size 0.8 0.8) (layers F.Cu F.Paste F.Mask))
-  (pad #{pa} smd rect (at -0.043898 -0.020304 90) (size 1.4 0.4) (layers F.Cu F.Paste F.Mask))
-  (pad #{pk} smd rect (at 1.056102 -0.020304 90) (size 1.4 0.4) (layers F.Cu F.Paste F.Mask))
-  (pad #{pk} smd rect (at 1.306102 -0.020304 90) (size 0.8 0.8) (layers F.Cu F.Paste F.Mask))
+
+
+  (pad #{pa} smd rect (at -0.55 0 90) (size 0.8 0.7) (drill (offset 0 -0.2)) (layers F.Cu F.Paste F.Mask))
+  (pad #{pa} smd rect (at -0.55 0 90) (size 1.1 0.5) (layers F.Cu F.Paste F.Mask))
+  (pad #{pk} smd rect (at 0.55 0 90) (size 1.1 0.5) (layers F.Cu F.Paste F.Mask))
+  (pad #{pk} smd rect (at 0.55 0 90) (size 0.8 0.7) (drill (offset 0 0.2)) (layers F.Cu F.Paste F.Mask))
+
+
+
 	"
 		puts n.gsub( /\((start|end|at)\s+([-0-9.]+)\s+([-0-9.]+)/ ) { |match|
 			"(#{$1} #{xx+$2.to_f} #{yy+$3.to_f}" }
